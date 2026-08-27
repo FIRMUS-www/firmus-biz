9627eb346ae29059c2f8ace94c07d7753645d983
FIRMUS-www <spolkafirmus@gmail.com>
2026-08-27 12:32:13 +0200

    executor(local_chatgpt): FIRMUS-BIZ-RYCZALT-HERO-WIDTH-HOTFIX-20260827

--- script.js
+++ script.js
@@ -285,7 +285,7 @@ function initRyczaltCampaignLoader() {
   if (!document.querySelector('link[data-ryczalt-styles]')) {
     const link = document.createElement("link");
     link.rel = "stylesheet";
-    link.href = "/ryczalt.css?v=20260827-2";
+    link.href = "/ryczalt.css?v=20260827-3";
     link.dataset.ryczaltStyles = "true";
     document.head.appendChild(link);
   }
